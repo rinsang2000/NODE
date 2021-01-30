eea24e87a89cfafbf8950f1326e79561887470df
rinsang2000 <74318250+rinsang2000@users.noreply.github.com>
2021-01-30 18:17:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const me= {
+  name: "Sang",
+  age: 20
 }
 
-export default App;
+export default function App(props){
+  const showfromList =(list) => {
+    return list.map(item =>
+      <div>dsada</div>)
+  }
+  return(
+    <div className="layout">
+      <div className="layout_left">
+        <div className="avatar">
+          <img src="https://baominh.tech/wp-content/uploads/2020/09/nhan-dan-facebook.png"></img>
+          <div className="intro_basic">
+            <h2>Hello</h2>
+            <h3>My name {me.name}</h3>
+          </div>
+        </div>
+      </div>
+      <div className="layout_right">
+        <div className="intro">
+          <h3>introducing ing youself self</h3>
+          <h3>introducing with text boxes</h3>
+        </div>
+        <div className="line">
+          <div className="line-center" ></div>
+          <div className="cicle left" ></div>
+          <div className="cicle right" ></div>
+        </div>
+        <div className="texboxes-container">
+          <div className="item-container">
+            <div className="item-content">
+              <h3>Text here1</h3>
+              <p>Lorem ipsum dolor sit amet</p>
+              </div>
+            
+                    
+          </div>
+          <div className="item-container">
+            <div className="item-content">
+              <h3>Text here1</h3>
+              <p>Lorem ipsum dolor sit amet</p>
+              </div>
+            
+                    
+          </div>
+          <div className="item-container">
+            <div className="item-content">
+              <h3>Text here1</h3>
+              <p>Lorem ipsum dolor sit amet</p>
+              </div>
+            
+                    
+          </div>
+          <div className="item-container">
+            <div className="item-content">
+              <h3>Text here1</h3>
+              <p>Lorem ipsum dolor sit amet</p>
+              </div>
+            
+                    
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
